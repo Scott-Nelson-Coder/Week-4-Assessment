ea1c17da8756b1103427e2744841efc17527401d
Scott Nelson <scottninlv@yahoo.com>
2023-08-04 09:22:53 -0700

    set up

---
+++ client/main.js
@@ -0,0 +1,13 @@
+const complimentBtn = document.getElementById("complimentButton")
+const fortuneBtn = document.getElementById("fortuneButton")
+
+
+const getCompliment = () => {
+    axios.get("http://localhost:4000/api/compliment/")
+        .then(res => {
+            const data = res.data;
+            alert(data);
+    });
+};
+
+complimentBtn.addEventListener('click', getCompliment)
---
+++ server/index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+
+
+const { getCompliment } = require('./controller')
+
+
+app.get("/api/compliment", getCompliment);
+
+
+
+
+app.listen(4000, () => console.log("------Party on server 4000------"));
